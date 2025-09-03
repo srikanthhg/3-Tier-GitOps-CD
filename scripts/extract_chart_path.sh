79126f93646ec175975ed431cc96e2755e64f79e
Vajrapu Srikanth <95075435+srikanthhg@users.noreply.github.com>
2025-09-03 21:18:31 +0530

    Update extract_chart_path.sh

--- scripts/extract_chart_path.sh
+++ scripts/extract_chart_path.sh
@@ -1,24 +1,24 @@
 count=$(cat modified_files.txt | wc -l)
 
 if [ "$count" -eq 1 ]; then
-result=$(cat modified_files.txt | awk -F '/' '{print $1"/"$2}')
-echo $result
+    result=$(cat modified_files.txt | awk -F '/' '{print $1"/"$2}')
+    echo $result
 else
 
-cat modified_files.txt | awk '
-
-BEGIN { FS="/" }
-{
-    prefix = $1 "/" $2
-    if (NR ==1) {
-        common = prefix
-    } else if (common != prefix) {
-        common = ""
+    cat modified_files.txt | awk '
+    
+    BEGIN { FS="/" }
+    {
+        prefix = $1 "/" $2
+        if (NR ==1) {
+            common = prefix
+        } else if (common != prefix) {
+            common = ""
+        }
     }
-}
-END {
-    if (common != "") {
-        print common
-    }
-}'
+    END {
+        if (common != "") {
+            print common
+        }
+    }'
 fi
